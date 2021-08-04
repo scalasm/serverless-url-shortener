773f92a2e2a4d1cc3f3d5887d2f687d4e5bb7339
Mario Scalas <mario.scalas@gmail.com>
2021-08-04 11:22:47 +0200

    Added header (with logo) and footer
    It is not perfect but at least it is working!

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,19 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    
     MatSliderModule,
+    MatToolbarModule,
 
     AmplifyUIAngularModule 
   ],
