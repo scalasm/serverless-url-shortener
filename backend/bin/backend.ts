75603f1b7ce0ee10b110aae5f71051b79c1a59c1
Mario Scalas <mario.scalas@gmail.com>
2021-11-11 22:46:20 +0000

    Temporarily disable the CI/CD pipeline and web frontend deployment.
    This will be re-enabled using a multistage pipeline (CDKPipelines have also been deprecated).

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -1,32 +1,38 @@
 #!/usr/bin/env node
 
-// Copyright Mario Scalas 2020. All Rights Reserved.
+// Copyright Mario Scalas 2021. All Rights Reserved.
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
-import { ZoorlPipelineStack } from "../lib/zoorl-pipeline-stack";
+import { ZoorlPipelineStack } from "../lib/cicd/zoorl-pipeline-stack";
 import { WebFrontendStack } from "../lib/web-frontend-stack";
+import { BackendStack } from "../lib/backend-stack";
 
 const app = new cdk.App();
 
 // If you want to support multiple environments, this is where to intervene
 const currentEnvironment = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 
-new ZoorlPipelineStack(app, `${currentEnvironment.region}-zoorl-pipeline-stack`, {
+new BackendStack(app, "zoorl-backend-stack", {
   // This is the environment where the CI/CD pipeline will be actually created
-  env: currentEnvironment,
-  // This is the environment where the application stack (e.g., APIs, DynamoDB tables, ...) will be deployed
-  stagingEnv: currentEnvironment
+  env: currentEnvironment
 });
 
-const stages = app.node.tryGetContext("deploymentStages")
+// new ZoorlPipelineStack(app, `${currentEnvironment.region}-zoorl-pipeline-stack`, {
+//   // This is the environment where the CI/CD pipeline will be actually created
+//   env: currentEnvironment,
+//   // This is the environment where the application stack (e.g., APIs, DynamoDB tables, ...) will be deployed
+//   stagingEnv: currentEnvironment
+// });
 
-new WebFrontendStack(app, `${currentEnvironment.region}-zoorl-webfrontend-stack`, {
-  env: currentEnvironment,
-  zoneName: stages["staging"]["zoneName"],
-  subdomain: stages["staging"]["subdomain"]
-});
+// const stages = app.node.tryGetContext("deploymentStages")
+
+// new WebFrontendStack(app, `${currentEnvironment.region}-zoorl-webfrontend-stack`, {
+//   env: currentEnvironment,
+//   zoneName: stages["staging"]["zoneName"],
+//   subdomain: stages["staging"]["subdomain"]
+// });
 
 app.synth();
