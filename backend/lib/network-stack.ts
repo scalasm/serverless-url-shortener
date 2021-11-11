52e891c8994091a482f713bac6bb3408bf49e186
Mario Scalas <mario.scalas@gmail.com>
2021-11-11 22:50:18 +0000

    Separated CI/CD infrastructure and add network stack.
    The network stack will host the computing infrastructure. Shared stack renamed to Auth stack for matching its purpose. Minor renames and refactorings.

---
+++ backend/lib/network-stack.ts
@@ -0,0 +1,22 @@
+// Copyright Mario Scalas 2021. All Rights Reserved.
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+
+/**
+ * Network stack for hosting the application.
+ */
+export class NetworkStack extends cdk.Stack {
+
+  readonly vpc: ec2.IVpc;
+
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.vpc = new ec2.Vpc(this, "vpc", {
+      natGateways: 1
+    })
+  }
+}
--- backend/lib/web-frontend-stack.ts
+++ backend/lib/web-frontend-stack.ts
@@ -1,4 +1,4 @@
-// Copyright Mario Scalas 2020. All Rights Reserved.
+// Copyright Mario Scalas 2021. All Rights Reserved.
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
